9990d08fd0ad9ca0e12bd7e2524b521d87c2667d
Kateryna Chelpan <catychelpan@gmail.com>
2024-05-20 18:53:39 +0300

    Created Table for projects and pagination functionality

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -11,5 +12,15 @@ class Project extends Model
 
     public function tasks() {
         return $this->hasMany(Task::class);
+    }
+
+    public function createdBy() {
+        return $this->belongsTo(User::class, 'created_by');
+
+    }
+
+    public function updatedBy() {
+        return $this->belongsTo(User::class, 'updated_by');
+
     }
 }
