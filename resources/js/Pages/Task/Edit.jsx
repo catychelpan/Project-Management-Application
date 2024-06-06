f6e99d7d9fccc88dcc8d73fda110614598d27721
Kateryna Chelpan <catychelpan@gmail.com>
2024-06-06 20:44:38 +0300

    Implemented CRUD for tasks

--- resources/js/Pages/Task/Edit.jsx
+++ resources/js/Pages/Task/Edit.jsx
@@ -6,15 +6,18 @@ import InputError from "@/Components/InputError";
 import TextAreaInput from "@/Components/TextAreaInput";
 import SelectInput from "@/Components/SelectInput";
 
-export default function Edit({auth, project}) {
+export default function Edit({auth, task, projects, users}) {
 
 
     const {data, setData, post, errors, reset} = useForm({
         image:"",
-        name: project.name || "",
-        status: project.status || "",
-        description: project.description || "",
-        due_date: project.due_date || "",
+        name: task.name || "",
+        status: task.status || "",
+        description: task.description || "",
+        due_date: task.due_date || "",
+        project_id: task.project_id || "",
+        priority: task.priority || "",
+        assigned_user_id: task.assigned_user_id || "",
         _method: 'PUT'
     })
 
@@ -23,7 +26,7 @@ export default function Edit({auth, project}) {
 
 
         //needs to know what data should be passed to the server
-        post(route('project.update', project.id))
+        post(route('task.update', task.id))
     }
 
 
@@ -33,24 +36,44 @@ export default function Edit({auth, project}) {
         header={
             <div className="flex justify-between items-center">
               <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Edit Project "{project.name}""
+                Edit Task "{task.name}""
               </h2>
             </div>
           }
         >
-            <Head title="Projects" />
+            <Head title="Tasks" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
 
                         <form className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg" onSubmit={onSubmit}>
-                            {project.image_path && <div className="flex justify-center items-center mb-4">
-                                <img src={project.image_path} className="w-64" alt="Project image" /></div>}
-                            <div>
-                                <InputLabel htmlFor="project_image_path" value="Project Image" />
+                            {task.image_path && <div className="flex justify-center items-center mb-4">
+                                <img src={task.image_path} className="w-64" alt="Task image" /></div>}
+                            <div className="mt-4">
+                                <InputLabel htmlFor="task_project_id" value="Project" />
+
+                                <SelectInput
+                                name="project_id"
+                                id="task_project_id"
+                                value={data.project_id}
+                                className="mt-1 block w-full"
+                                onChange={(e) => setData("project_id", e.target.value)}
+                                >
+                                <option value="">Select Project</option>
+                                {projects.data.map((project) => (
+                                    <option value={project.id} key={project.id}>{project.name}</option>
+                                ))}
+                                </SelectInput>
+
+                                <InputError message={errors.project_id} className="mt-2" />
+                            </div>
+
+
+                            <div className="mt-4">
+                                <InputLabel htmlFor="task_image_path" value="Task Image" />
                                 <TextInput
-                                id="project_image_path"
+                                id="task_image_path"
                                 type="file"
                                 name="image"
                                 className="mt-1 block w-full"
@@ -59,10 +82,11 @@ export default function Edit({auth, project}) {
                                 <InputError message={errors.image} className="mt-2"/>
                             </div>
 
+
                             <div className="mt-4">
-                                <InputLabel htmlFor="project_name" value="Project Name" />
+                                <InputLabel htmlFor="task_name" value="Task Name" />
                                 <TextInput
-                                id="project_name"
+                                id="task_name"
                                 type="text"
                                 name="name"
                                 value={data.name}
@@ -73,14 +97,15 @@ export default function Edit({auth, project}) {
                                 <InputError message={errors.name} className="mt-2"/>
                             </div>
 
+
                             <div className="mt-4">
                                 <InputLabel
-                                    htmlFor="project_description"
-                                    value="Project Description"
+                                    htmlFor="task_description"
+                                    value="Task Description"
                                 />
 
                                 <TextAreaInput
-                                    id="project_description"
+                                    id="task_description"
                                     name="description"
                                     value={data.description}
                                     className="mt-1 block w-full"
@@ -90,14 +115,15 @@ export default function Edit({auth, project}) {
                                 <InputError message={errors.description} className="mt-2" />
                             </div>
 
+
                             <div className="mt-4">
                                 <InputLabel
-                                htmlFor="project_due_date"
-                                value="Project Deadline"
+                                htmlFor="task_due_date"
+                                value="Task Deadline"
                                 />
 
                                 <TextInput
-                                id="project_due_date"
+                                id="task_due_date"
                                 type="date"
                                 name="due_date"
                                 value={data.due_date}
@@ -108,13 +134,14 @@ export default function Edit({auth, project}) {
                                 <InputError message={errors.due_date} className="mt-2" />
                             </div>
 
+
                             <div className="mt-4">
-                                <InputLabel htmlFor="project_status" value="Project Status" />
+                                <InputLabel htmlFor="task_status" value="Task Status" />
 
                                 <SelectInput
                                 name="status"
-                                id="project_status"
-                                defaultValue={data.status}
+                                id="task_status"
+                                value={data.status}
                                 className="mt-1 block w-full"
                                 onChange={(e) => setData("status", e.target.value)}
                                 >
@@ -124,12 +151,52 @@ export default function Edit({auth, project}) {
                                 <option value="completed">Completed</option>
                                 </SelectInput>
 
-                                <InputError message={errors.project_status} className="mt-2" />
+                                <InputError message={errors.task_status} className="mt-2" />
                             </div>
+
+
+                            <div className="mt-4">
+                                <InputLabel htmlFor="task_priority" value="Task Priority" />
+
+                                <SelectInput
+                                name="priority"
+                                id="task_priority"
+                                value={data.priority}
+                                className="mt-1 block w-full"
+                                onChange={(e) => setData("priority", e.target.value)}
+                                >
+                                <option value="">Select Priority</option>
+                                <option value="low">Low</option>
+                                <option value="medium">Medium</option>
+                                <option value="high">High</option>
+                                </SelectInput>
+
+                                <InputError message={errors.priority} className="mt-2" />
+                            </div>
+
+                            <div className="mt-4">
+                                <InputLabel htmlFor="task_assigned_user" value="Assigned User" />
+
+                                <SelectInput
+                                name="assigned_user_id"
+                                id="task_assigned_user"
+                                value={data.assigned_user_id}
+                                className="mt-1 block w-full"
+                                onChange={(e) => setData("assigned_user_id", e.target.value)}
+                                >
+                                <option value="">Select User</option>
+                                {users.data.map((user) => (
+                                    <option value={user.id} key={user.id}>{user.name}</option>
+                                ))}
+                                </SelectInput>
+
+                                <InputError message={errors.assigned_user_id} className="mt-2" />
+                            </div>
+
 
                             <div className="mt-4 text-right">
                                 <Link
-                                href={route("project.index")}
+                                href={route("task.index")}
                                 className="bg-gray-100 py-[7px] px-4 text-gray-800 rounded shadow transition-all hover:bg-gray-200 mr-2"
                                 >
                                 Cancel
--- resources/js/Pages/Task/TasksTable.jsx
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -46,6 +46,16 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
 
     }
 
+    const deleteTask = (task) => {
+
+        if (!window.confirm('Are you sure you want to delete the task?')) {
+            return;
+        }
+
+        router.delete(route('task.destroy', task.id))
+
+    }
+
     return (
         <>
             <div className="overflow-auto">
@@ -147,11 +157,13 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
                                 <td className="px-3 py-2 text-nowrap">{task.created_at}</td>
                                 <td className="px-3 py-2 text-nowrap">{task.due_date}</td>
                                 <td className="px-3 py-2">{task.createdBy.name}</td>
-                                <td className="px-3 py-2">
-                                <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1" href={route('task.edit', task.id)}>Edit</Link>
-                                <Link href={route("task.destroy", task.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
-                                        Delete
+                                <td className="px-3 py-2 text-nowrap">
+                                    <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1" href={route('task.edit', task.id)}>
+                                        Edit
                                     </Link>
+                                    <button onClick={(e) => deleteTask(task)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
+                                        Delete
+                                    </button>
                                 </td>
                             </tr>
 
